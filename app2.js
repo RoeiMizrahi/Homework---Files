283ed9ff4645eef9a12b59718a8e61b88ba0c8ee
RoeiMizrahi <roeimi1063@gmail.com>
2025-02-05 12:57:16 +0200

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const inputFiles = [
+  "text1.txt",
+  "text2.txt",
+  "text3.txt",
+  "text4.txt",
+  "text5.txt",
+  "text6.txt",
+  "text7.txt",
+  "text8.txt",
+  "text9.txt",
+  "text10.txt",
+];
+const outputFile = "output3.txt";
+async function mergeFiles() {
+  const writer = fs.createWriteStream(outputFile);
+  for (let i = 0; i < inputFiles.length; i++) {
+    const fileName = inputFiles[i];
+    const linesToRead = i + 1;
+    const fileContent = fs.readFileSync(fileName, "utf8");
+    const lines = fileContent.split("\n");
+    if (lines.length >= linesToRead) {
+      for (let j = 0; j < linesToRead; j++) writer.write(lines[j] + "\n");
+    }
+  }
+}
+mergeFiles();
